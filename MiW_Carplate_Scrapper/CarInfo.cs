fe8a487679e749a29fb3c4f0f10f9d47fa394c00
Przem0s84 <przemyslaw.wacek202@gmail.com>
2024-06-03 16:52:51 +0200

    Dodanie Scrapera do repozytorium

---
+++ MiW_Carplate_Scrapper/CarInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace CarPlatesScrapper
+{
+    public class CarInfo
+    {
+        public string PlateId { get; set; }
+        public IEnumerable<string> Comments { get; set; }
+    }
+}
---
+++ MiW_Carplate_Scrapper/PlateService.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+
+namespace CarPlatesScrapper
+{
+    public class PlateService:IPlateService
+    {   
+        private readonly HttpClient _httpClient;
+        public PlateService(HttpClient httpClient) 
+        { 
+            _httpClient = httpClient;
+        }
+        public async Task<string> GetPlateIdAsync(IFormFile image)
+        {
+            var apiPath = "http://127.0.0.1:8000/recognize";
+            using var content = new MultipartFormDataContent();
+            using var fileStream = image.OpenReadStream();
+            using var streamContent = new StreamContent(fileStream);
+            streamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(image.ContentType);
+            content.Add(streamContent, "file", image.FileName);
+            var response = await _httpClient.PostAsync(apiPath,content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var result = responseContent.Trim('"');
+            return result;
+        }
+
+        public async Task<CarInfo> GetCarInfoAsync(string plateId)
+        {
+            var path = $"https://tablica-rejestracyjna.pl/{plateId}";
+            var web = new HtmlWeb();
+            web.OverrideEncoding = Encoding.UTF8;
+            var doc = web.Load(path);
+
+            var middleColumn = doc.QuerySelector(".col-md-8");
+            var commentsObj = middleColumn.QuerySelectorAll(".comment .text");
+            var comments = commentsObj.Select(c => c.InnerText).ToList();
+            var carInfo = new CarInfo();
+            carInfo.PlateId = plateId;
+            carInfo.Comments = comments.Take(20).ToList();
+
+            return carInfo;
+        }
+
+
+    }
+}
